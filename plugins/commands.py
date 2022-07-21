b73d283962a721dcf994fc2a0e7906dc3356e66d
Sukhmanpreet <101450549+Sukhj12@users.noreply.github.com>
2022-07-21 20:17:30 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -45,9 +45,9 @@ async def start(client, message):
             InlineKeyboardButton('✰ ᗩᗪᗪ ᗰᗴ TO YOᑌᖇ ᘜᖇOᑌᑭ ✰', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('✰ Տᗴᗩᖇᑕᕼ ✰', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('✰ ᑕᕼᗩᑎᑎᗴᒪ ✰', url='https://t.me/TeamEvamaria')
-            InlineKeyboardButton('✰ ᕼᗴᒪᑭ ✰', callback_data='help'),
+            InlineKeyboardButton('✰ ᑕᕼᗩᑎᑎᗴᒪ ✰', url='https://t.me/TeamEvamaria'),
             ],[
+            InlineKeyboardButton('✰ ᕼᗴᒪᑭ ✰', callback_data='help')
             InlineKeyboardButton('✰ ᗰY ᘜᖇOᑌᑭ ✰', url='https://t.me/TeamEvamaria')
             InlineKeyboardButton('✰ ᗩᗷOᑌT ✰', callback_data='about')
         ]]
@@ -92,9 +92,9 @@ async def start(client, message):
             InlineKeyboardButton('✰ ᗩᗪᗪ ᗰᗴ TO YOᑌᖇ ᘜᖇOᑌᑭ ✰', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('✰ Տᗴᗩᖇᑕᕼ ✰', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('✰ ᑕᕼᗩᑎᑎᗴᒪ ✰', url='https://t.me/TeamEvamaria')
-            InlineKeyboardButton('✰ ᕼᗴᒪᑭ ✰', callback_data='help'),
+            InlineKeyboardButton('✰ ᑕᕼᗩᑎᑎᗴᒪ ✰', url='https://t.me/TeamEvamaria'),
             ],[
+            InlineKeyboardButton('✰ ᕼᗴᒪᑭ ✰', callback_data='help')
             InlineKeyboardButton('✰ ᗰY ᘜᖇOᑌᑭ ✰', url='https://t.me/TeamEvamaria')
             InlineKeyboardButton('✰ ᗩᗷOᑌT ✰', callback_data='about')
         ]]
