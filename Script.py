fb259d28f30a1a42a49cd2deecd9ebdff7818e3c
Sukhmanpreet <101450549+Sukhj12@users.noreply.github.com>
2022-07-22 06:41:56 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,15 +1,15 @@
 class script(object):
-    START_TXT = """𝙷𝙴𝙻𝙾 {},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
-    HELP_TXT = """𝙷𝙴𝚈 {}
-𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
+    START_TXT = """ᕼᗴᒪO {},
+ᗰY ᑎᗩᗰᗴ IՏ <a href=https://t.me/{}>{}</a>, I ᑕᗩᑎ ᑭᖇOᐯIᗪᗴ ᗰOᐯIᗴՏ, ᒍᑌՏT ᗩᗪᗪ ᗰᗴ TO YOᑌ ᘜᖇOᑌᑭ ᗩᑎY ᗴᑎᒍOY"""
+    HELP_TXT = """ᕼᗴY {}
+ᕼᗴᖇᗴ IՏ Tᕼᗴ ᕼᗴᒪᑭ ᖴOᖇ ᗰY ᑕOᗰᗰᗩᑎᗪ."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
+✯ ᑕᖇᗴᗩTᗴᖇ: <a href=https://t.me/Sukhmankaler>Oᗯᑎᗴᖇ</a>
+✯ ᒪIᗷᖇᗩᖇY: ᑭYᖇO
+✯ ᒪᗩᑎᘜᑌᗩᘜᗴ: ᑭYTᕼOᑎ 3
+✯ ᗪᗩTᗩᗷᗩՏᗴ: ᗰOᑎᘜO ᗪᗷ
+✯ ᗷOT Տᗴᖇᐯᗴᖇ: ᕼᗴᖇOKᑌ
+✯ ᗷᑌIᒪᗪ ՏTᗩTᑌՏ: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
 - Eva Maria is a open source project. 
 - Source - https://github.com/EvamariaTG/EvaMaria  
