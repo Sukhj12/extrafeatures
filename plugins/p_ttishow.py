d1a8e16c14a2ae5fc9b951e071195eb7fb8d3440
Sukhmanpreet <101450549+Sukhj12@users.noreply.github.com>
2022-07-24 19:04:36 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -67,7 +67,7 @@ async def leave_a_chat(bot, message):
         chat = chat
     try:
         buttons = [[
-            InlineKeyboardButton('Support', url=f'https://t.me/{SUPPORT_CHAT}')
+            InlineKeyboardButton('Admine', url=f'https://t.me/Sukhmankaler')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await bot.send_message(
@@ -106,7 +106,7 @@ async def disable_chat(bot, message):
     await message.reply('Chat Successfully Disabled')
     try:
         buttons = [[
-            InlineKeyboardButton('Support', url=f'https://t.me/{SUPPORT_CHAT}')
+            InlineKeyboardButton('Admine', url=f'https://t.me/Sukhmankaler')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await bot.send_message(
