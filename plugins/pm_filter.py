a4baffd34b1516582fe4c0708bb05e10698f55c7
Sukhmanpreet <101450549+Sukhj12@users.noreply.github.com>
2022-07-28 14:31:29 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -120,7 +120,7 @@ async def next_page(bot, query):
 async def advantage_spoll_choker(bot, query):
     _, user, movie_ = query.data.split('#')
     if int(user) != 0 and query.from_user.id != int(user):
-        return await query.answer("okDa", show_alert=True)
+        return await query.answer("🙏𝗦𝗘𝗔𝗥𝗖𝗛 𝗬𝗢𝗨𝗥 𝗢𝗪𝗡 𝗙𝗜𝗟𝗘🙏   आप खुदकी मूवी search करो 🧡🧡", show_alert=True)
     if movie_ == "close_spellcheck":
         return await query.message.delete()
     movies = SPELL_CHECK.get(query.message.reply_to_message.message_id)
