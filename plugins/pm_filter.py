e9b4bcbfa955f4a64885abf96797955c4b8a78f4
Sukhmanpreet <101450549+Sukhj12@users.noreply.github.com>
2022-08-05 14:14:46 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -136,8 +136,8 @@ async def advantage_spoll_choker(bot, query):
             await auto_filter(bot, query, k)
         else:
             k = await query.message.edit('<b>TᕼIՏ ᗰOᐯIᗴ ᑎOT ᑌᑭᒪOᗩᗪ ᖇᑌᑫᑌᗴՏT TO ᗩᗪᗰIᑎᗴ ✰✰✰✰ 👉[ᑕᒪIᑕK ᕼᗴᖇᗴ](https://t.me/RQ_ADMINE)👈ᖴOᖇ ᖇᑌᑫᑌᗴՏT</b>')
-             await asyncio.sleep(20)
-             await k.delete()
+            await asyncio.sleep(20)
+            await k.delete()
 
             
 
