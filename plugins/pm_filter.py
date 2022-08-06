74586352870bfb78b4cfbde5a6505d2060b481de
Sukhmanpreet <101450549+Sukhj12@users.noreply.github.com>
2022-08-06 16:18:01 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -146,8 +146,8 @@ async def advantage_spoll_choker(bot, query):
                 ]
             ]
         )
+    )
 
-    
 @Client.on_callback_query()
 async def cb_handler(client: Client, query: CallbackQuery):
     if query.data == "close_data":
