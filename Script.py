c4bdf6a99d651fff399280afd4ea350c0da31596
Sukhmanpreet <101450549+Sukhj12@users.noreply.github.com>
2022-08-11 11:10:26 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,15 +1,15 @@
 class script(object):
-    START_TXT = """ᕼᗴᒪO {},
-ᗰY ᑎᗩᗰᗴ IՏ <a href=https://t.me/{}>{}</a>, I ᑕᗩᑎ ᑭᖇOᐯIᗪᗴ ᗰOᐯIᗴՏ, ᒍᑌՏT ᗩᗪᗪ ᗰᗴ TO YOᑌ ᘜᖇOᑌᑭ ᗩᑎY ᗴᑎᒍOY"""
-    HELP_TXT = """ᕼᗴY {}
-ᕼᗴᖇᗴ IՏ Tᕼᗴ ᕼᗴᒪᑭ ᖴOᖇ ᗰY ᑕOᗰᗰᗩᑎᗪ."""
+    START_TXT = """𝙷𝙴𝙻𝙻𝙾 {},
+𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝚃𝙾 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿"""
+    HELP_TXT = """𝙷𝙴𝚈 {}
+𝙷𝙴𝚁𝙴 𝙸𝚂 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙲𝙾𝙼𝙼𝙰𝙽𝙳."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ ᑕᖇᗴᗩTᗴᖇ: <a href=https://t.me/Sukhmankaler>Oᗯᑎᗴᖇ</a>
-✯ ᒪIᗷᖇᗩᖇY: ᑭYᖇO
-✯ ᒪᗩᑎᘜᑌᗩᘜᗴ: ᑭYTᕼOᑎ 3
-✯ ᗪᗩTᗩᗷᗩՏᗴ: ᗰOᑎᘜO ᗪᗷ
-✯ ᗷOT Տᗴᖇᐯᗴᖇ: ᕼᗴᖇOKᑌ
-✯ ᗷᑌIᒪᗪ ՏTᗩTᑌՏ: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
+⚕️ 𝙲𝚁𝙴𝙰𝚃𝙴𝚁: <a href=https://t.me/Sukhmankaler>Oᗯᑎᗴᖇ</a>
+⚕️ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾
+⚕️ 𝙻𝙰𝙽𝙶𝚄𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽
+⚕️ 𝙳𝙰𝚃𝙰𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙳𝙾 𝙳𝙱
+⚕️ 𝙱𝙾𝚃 𝚂𝙴𝚁𝙴𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
+⚕️ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
 - Eva Maria is a open source project. 
 - Source - https://github.com/EvamariaTG/EvaMaria  
@@ -91,11 +91,11 @@ This module only works for my admins
 • /unban  - <code>to unban a user.</code>
 • /channel - <code>to get list of total connected channels</code>
 • /broadcast - <code>to broadcast a message to all users</code>"""
-    STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
-★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
-★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
+    STATUS_TXT = """⚕️ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
+⚕️ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
+⚕️ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
+⚕️ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
+⚕️ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
     LOG_TEXT_G = """#NewGroup
 Group = {}(<code>{}</code>)
 Total Members = <code>{}</code>
