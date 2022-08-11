25cf7f79e580d2b5e417b4949fe458db9f67260b
Sukhmanpreet <101450549+Sukhj12@users.noreply.github.com>
2022-08-11 10:32:57 +0530

    Create pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -206,7 +206,7 @@ async def advantage_spoll_choker(bot, query):
             k = (movie, files, offset, total_results)
             await auto_filter(bot, query, k)
         else:
-            k = await query.message.edit('<b>TᕼIՏ ᗰOᐯIᗴ ᑎOT ᑌᑭᒪOᗩᗪ ᖇᑌᑫᑌᗴՏT TO ᗩᗪᗰIᑎᗴ ✰✰✰✰ 👉[ᑕᒪIᑕK ᕼᗴᖇᗴ](https://t.me/RQ_ADMINE)👈ᖴOᖇ ᖇᑌᑫᑌᗴՏT</b>')
+            k = await query.message.edit('<b>𝚃𝙷𝙸𝚂 𝙼𝙾𝚅𝙸𝙴 𝙽𝙾𝚃 𝚄𝙿𝙻𝙾𝙰𝙳 𝚁𝚄𝚀𝙸𝙴𝚂𝚃 𝚃𝙾 𝙰𝙳𝙼𝙸𝙽𝙴 👉♻️ @DmAdmine ♻️</b>')
             await asyncio.sleep(20)
             await k.delete()
             reply_markup=InlineKeyboardMarkup(
