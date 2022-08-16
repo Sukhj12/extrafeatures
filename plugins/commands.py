2bff02a95c508d90431309a835245eff4c22f588
Sukhmanpreet <101450549+Sukhj12@users.noreply.github.com>
2022-08-16 16:24:17 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -19,7 +19,7 @@ logger = logging.getLogger(__name__)
 BATCH_FILES = {}
 
 @Client.on_message(filters.command("start") & filters.incoming & ~filters.edited)
-async def start(client, message):
+async def start(client, message: pyrogram.types.Message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
             [
